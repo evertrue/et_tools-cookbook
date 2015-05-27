6e7947914c77aab05402fa594a84f6b737f47ec8
Eric Herot <eric.github@herot.com>
2015-05-27 15:57:08 -0400

    Indentation and sorting cleanup in package list

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,29 @@
-common_packages = %w[ iotop strace mc iptraf tree ]
+common_packages = %w(iotop
+                     iptraf
+                     mc
+                     strace
+                     tree)
 
 case node['platform']
 when "debian","ubuntu"
-  default['tools']['packages'] = common_packages + %w[ dnsutils git-core links locate mailutils sysstat zip ntop iftop htop hping3 nload ]
+  default['tools']['packages'] = common_packages +
+                                 %w(dnsutils
+                                    git-core
+                                    htop
+                                    hping3
+                                    iftop
+                                    links
+                                    locate
+                                    mailutils
+                                    nload
+                                    ntop
+                                    sysstat
+                                    zip)
 when "redhat","centos","scientific","amazon"
-  default['tools']['packages'] = common_packages + %w[ bc bind-utils elinks git mailx ]
+  default['tools']['packages'] = common_packages +
+                                 %w(bc
+                                    bind-utils
+                                    elinks
+                                    git
+                                    mailx)
 end
