ae5830e90bbd77b090c6793fcf3c6f2d91ecb2ba
Eric Herot <eric.github@herot.com>
2015-05-28 15:03:50 -0400

    Only try to install jq on 14.04 systems

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,25 +6,27 @@ common_packages = %w(iotop
 
 case node['platform_family']
 when "debian"
-  default['tools']['packages'] = common_packages +
-                                 %w(dnsutils
-                                    git-core
-                                    htop
-                                    hping3
-                                    iftop
-                                    jq
-                                    links
-                                    locate
-                                    mailutils
-                                    nload
-                                    ntop
-                                    sysstat
-                                    zip)
+  pkgs = common_packages +
+         %w(dnsutils
+            git-core
+            htop
+            hping3
+            iftop
+            links
+            locate
+            mailutils
+            nload
+            ntop
+            sysstat
+            zip)
+  pkgs << 'jq' if node['platform_version'].to_f >= 14.04
 when "redhat"
-  default['tools']['packages'] = common_packages +
-                                 %w(bc
-                                    bind-utils
-                                    elinks
-                                    git
-                                    mailx)
+  pkgs = common_packages +
+         %w(bc
+            bind-utils
+            elinks
+            git
+            mailx)
 end
+
+default['tools']['packages'] = pkgs
